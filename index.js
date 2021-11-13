4c63022cf914c52d77e1a03d8afafa4a8d55c5d0
kembreyfarquhar <kembreyfarquhar@gmail.com>
2021-11-12 23:13:43 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const db = require("./db/dbConfig");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (_req, res) => {
+  res.send("Server up and running...");
+});
+
+server.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    if (!username || !password) {
+      res.status(400).json({ message: "must include username and password" });
+    }
+    const hash = bcrypt.hashSync(password);
+    const [newUser] = await db("users")
+      .insert({ username, password: hash })
+      .returning("*");
+    delete newUser.password;
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+server.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    if (!username || !password) {
+      res.status(400).json({ message: "must include username and password" });
+    }
+    const foundUser = await db("users").where({ username }).first();
+    if (!bcrypt.compareSync(foundUser.password, password)) {
+      res.status(400).json({ message: "invalid credentials" });
+    } else {
+      delete foundUser.password;
+      res.status(200).json(foundUser);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+const port = process.env.PORT || 4400;
+server.listen(port, () => console.log(`\n**Running on port ${port}**\n`));
---
+++ knexfile.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "pg",
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 6,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+    },
+    connection: {
+      database: process.env.DB_DEV_DATABASE,
+      user: process.env.DB_DEV_USER,
+      password: process.env.DB_DEV_PASSWORD,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./db/migrations",
+    },
+  },
+};
